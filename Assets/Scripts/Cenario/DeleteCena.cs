daea9710252964d32fbdc3bb5cf54c2d24a94b6c
GuilhermeHLSouza <guilherme.souza726@etec.sp.gov.br>
2024-09-01 19:02:14 -0300

    repetição do cenario inicial

---
+++ Assets/Scripts/Cenario/DeleteCena.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteCena : MonoBehaviour
+{
+    public GameObject personagem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        personagem = GameObject.Find("player").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (personagem.transform.position.x  <= transform.position.x - 70f)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("ruas/rua1"));
+        }
+    }
+}
--- Assets/Scripts/Movimentação_Personagem/MovCam.cs
+++ Assets/Scripts/Movimentação_Personagem/MovCam.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movPerCam : MonoBehaviour
+public class MovCam : MonoBehaviour
 {
-    Vector3 M;
+    public Vector3 M;
+    public float v;
+
     // Start is called before the first frame update
     void Start()
     {
-        M = new Vector3(16.31f, 3.441385f, -7.811185f);
         transform.position = M;
     }
 
     // Update is called once per frame
     void Update()
     {
-        M = new Vector3(0.01f, 0f, 0f);
+        M = new Vector3(v, 0f, 0f);
         transform.Translate(M);
     }
+
 }
