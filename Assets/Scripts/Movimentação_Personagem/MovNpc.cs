74bccc247cf97aead39bce793fac0de7ff82dece
ChristianVarandas <christian.varandas@etec.sp.gov.br>
2024-09-05 14:51:11 -0300

    adicionando movimento inicial dos npcs

---
+++ Assets/Scripts/Movimentação_Personagem/MovNpc.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovNpc : MonoBehaviour
+{
+    public Vector3 Mnpc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mnpc = new Vector3();
+        transform.position = Mnpc;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mnpc.x = -0.001f;
+        Mnpc.y = 0f;
+        Mnpc.z = 0f;
+        transform.Translate(Mnpc);
+    }
+}
