64bf7e15d55be6134d10652e6f0909c4f4fdb984
MicheliniDev <michelinidev@gmail.com>
2024-09-16 16:43:14 -0300

    Terminando a aba sobre nós

---
+++ Assets/perfis.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perfis : MonoBehaviour
+{
+    public void GamesClick()
+    {
+        Application.OpenURL("https://bsky.app/profile/michelinidev.bsky.social");
+    }
+}
