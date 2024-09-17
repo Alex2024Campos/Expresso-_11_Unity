ad2ccd495bbcfb3cf5d09bb818d5018946332838
GuilhermeHLSouza <guilherme.souza726@etec.sp.gov.br>
2024-09-17 19:43:14 -0300

    arrumando alguns bugs

--- Assets/Scripts/Movimentação_Personagem/MovNpc.cs
+++ Assets/Scripts/Movimentação_Personagem/MovNpc.cs
@@ -12,20 +12,17 @@ public class MovNpc : MonoBehaviour
     {
         spawn = GameObject.Find("Spawn").gameObject;
         Mnpc = spawn.transform.position;
-        transform.position = Mnpc;
+        //transform.position = Mnpc;
         transform.Rotate(0f, 90f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        {
             Mnpc.x = 0f;
             Mnpc.y = 0f;
             Mnpc.z = 5f * Time.deltaTime;
             transform.Translate(Mnpc);
-        }
         if (transform.position.x >= 17.45f){
             Destroy(this.gameObject);
         }
